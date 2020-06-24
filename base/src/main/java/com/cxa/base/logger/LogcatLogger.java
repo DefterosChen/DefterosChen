5800dc672c0f00c550ce7b5be347e67b0ad483ef
username <281663448@QQ.COM>
2020-06-24 10:36:22 +0800

    Caesar

--- base/src/main/java/com/cxa/base/logger/LogcatLogger.java
+++ base/src/main/java/com/cxa/base/logger/LogcatLogger.java
@@ -65,6 +65,7 @@ public class LogcatLogger implements ILogger {
         log(priority, tag, message);
     }
 
+    
 
     private String getStackTraceString(Throwable t) {
         // Don't replace this with Log.getStackTraceString() - it hides
