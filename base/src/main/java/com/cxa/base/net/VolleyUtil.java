0b468af3f6a5d95264e8211aa0a2f3e96bcbc37e
DefterosChen <281663448@QQ.COM>
2020-08-21 18:07:30 +0800

    Caesar

--- base/src/main/java/com/cxa/base/net/VolleyUtil.java
+++ base/src/main/java/com/cxa/base/net/VolleyUtil.java
@@ -67,7 +67,6 @@ public class VolleyUtil {
         req.setRetryPolicy(new DefaultRetryPolicy(40 * 1000, 1, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         mQueue.add(req);
     }
-
     /**
      * 添加默认的请求到队列中
      *
