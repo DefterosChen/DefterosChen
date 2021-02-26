476add2d82dfa2ebbc8c101807880277df444dbb
DefterosChen <281663448@qq.com>
2021-02-26 10:24:04 +0800

    123

--- base/src/main/java/com/cxa/base/net/GsonRequest.java
+++ base/src/main/java/com/cxa/base/net/GsonRequest.java
@@ -49,7 +49,7 @@ public class GsonRequest<T> extends Request<T> {
         mListener = listener;
         mMap = map;
     }
-    
+
     public GsonRequest(String url, Class<T> clazz, Listener<T> listener,
                        ErrorListener errorListener) {
         this(Method.GET, url, clazz, listener, errorListener,null);
